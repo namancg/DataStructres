3f345b887eb9422460de28e2625588c76079e5aa
namancg <naman.c@perfios.com>
2021-09-05 00:21:24 +0530

    REFACTORED: used linkedlist to implement orderedList

--- src/com/bridgelabz/dsprograms/OrderedList.java
+++ src/com/bridgelabz/dsprograms/OrderedList.java
@@ -1,48 +1,26 @@
 package com.bridgelabz.dsprograms;
 import java.util.*;
 import java.util.LinkedList;
-import com.bridelabz.hashmap.*;
+import com.bridgelabz.linkedlists.*;
 
 public class OrderedList {
-	public static LinkedList<Integer> obj = new LinkedList<Integer>(null);
 	public static void main(String[] args) 
 	{
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of number");
-		int size = sc.nextInt();
-		System.out.println("Enter " +size+ " number ");
-		int numbers[] = new int[size];
-		for(int index = 0; index<size;index++)
-		{
-			numbers[index] = sc.nextInt();
+		Integer[] numbers = { 10, 5, 3, 16, 5, 10, 12 };
+		Node<Integer> newNode;
+		SortedLinkedList<Integer> mySortedLinkedList = new SortedLinkedList<Integer>();
+		for (Integer number : numbers) {
+			if (mySortedLinkedList.search(number) == null) {
+				newNode = new Node<Integer>(number);
+				mySortedLinkedList.append(newNode);
+				System.out.println(number + " added");
+			} else {
+				mySortedLinkedList.delete(number);
+				System.out.println(number + " deleted");
+			}
 		}
-		for(int numberPresent : numbers) 
-		{
-			Node<Integer> node = new Node<Integer>(numberPresent);
-			obj.add(node);
-		}
-		System.out.println("which number has to be searched ??");
-		int numberToBeSearched = sc.nextInt();
-		searchNumber(numbers,numberToBeSearched);
-		sc.close();
+		mySortedLinkedList.printMyNodes();
 	}
 
-	private static void searchNumber(int[] numbers, int numberToBeSearched) {
-		System.out.println(numberToBeSearched);
-		INode<Integer> newNode = obj.find(numberToBeSearched);
-		if(newNode==null)
-		{
-			Node<Integer> node = new Node<Integer>(numberToBeSearched);
-			obj.append(node);
-			System.out.println(numberToBeSearched+ " Found ");
-				
-		}
-		else 
-		{
-			obj.delete(numberToBeSearched);
-			System.out.println("The number is deleted");
-		}
-		
-	}
 }
